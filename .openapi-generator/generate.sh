c9d260c12404f6c34031c3c4e0b70d3542454c08
AntoineDao <antoinedao1@gmail.com>
2020-07-31 09:13:14 +0100

    chore(openapi): use newly scoped npm package

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx openapi-generator generate \
+npx @openapitools/openapi-generator-cli generate \
   -i https://api.pollination.cloud/sdk_openapi.json \
   -t .openapi-generator/templates/typescript-axios \
   -g typescript-axios \
