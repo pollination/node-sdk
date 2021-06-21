9937a1d0afe33ad5a16c91e1968f7e9122429cc8
ladybugbot <releases@ladybug.tools>
2021-06-21 14:48:40 +0000

    upgrade SDK to v0.13.0

--- models/body-post-recipe-owner-recipes-post.ts
+++ models/body-post-recipe-owner-recipes-post.ts
@@ -15,19 +15,17 @@
 
 
 /**
- * Enumaration of allowable status strings
+ * 
  * @export
- * @enum {string}
+ * @interface BodyPostRecipeOwnerRecipesPost
  */
-export enum JobStatusEnum {
-    Created = 'Created',
-    PreProcessing = 'Pre-Processing',
-    Running = 'Running',
-    Failed = 'Failed',
-    Cancelled = 'Cancelled',
-    Completed = 'Completed',
-    Unknown = 'Unknown'
+export interface BodyPostRecipeOwnerRecipesPost {
+    /**
+     * 
+     * @type {any}
+     * @memberof BodyPostRecipeOwnerRecipesPost
+     */
+    _package: any;
 }
-
 
 
